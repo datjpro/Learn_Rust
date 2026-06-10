efaa2426dd17299fecadd0e1fbae610fb6406eae
Tô Phạm Thành Đạt <todat2207@gmail.com>
2026-06-10 21:09:07 +0700

    Part 1: Khái niệm

---
+++ variable/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // let mut x = 10;
+    // println!("x = {}", x);
+    // x = 20;
+    // println!("x = {}", x);
+
+    // const HANG_SO: u128 = 100_000_000_000_000;
+    // println!("HANG_SO = {}", HANG_SO);
+
+    //Shawdowing:
+    // let x = 10;
+    // println!("x = {}", x);
+    // let x = "ten";
+    // println!("x = {}", x);
+
+    //Data types:
+      // Scalar types
+      // Compound types
+    
+
+
+}
